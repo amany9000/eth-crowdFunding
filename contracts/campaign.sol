b5515495296baa3f531c118525a6140e9e0bdfff
amany9000 <201651007@iiitvadodara.ac.in>
2018-09-20 21:17:51 +0530

    Updated campaign.sol

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -1,12 +1,42 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
-    address public manager;
-    uint public minimumContrubution;
-    address[] public approvers;
     
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minContribution;
+    address[] public approvers;
+
+    modifier onlyManager{
+        require(msg.sender == manager);
+        _;
+    }
+
     function Campaign(uint minimum) public{
         manager = msg.sender;
-        minimumContrubution = minimum;
+        minContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value >= minContribution);
+        approvers.push(msg.sender);
+    }
+    
+    function createRequest(string description, uint value, address recipient) public onlyManager{
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+        
+        requests.push(newRequest);
     }
 }
