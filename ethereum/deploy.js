9200625d2bff6eb983e744cab308154a18ad74a6
Aman <amanyadavlko2502@gmail.com>
2018-09-28 10:59:37 +0530

    Added deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,3 +1,25 @@
 
+// This is a file to deploy the compiled factory contract at .build/CampaignStore.json
+const hdWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const wallet = require("truffle-hdwallet-provider")
+const compiledStore = require("../ethereum/build/CampaignStore.json");
+
+const provider = new hdWalletProvider(
+	"cousin wasp clip dynamic advance devote this million magic bean ceiling anger",
+	"https://rinkeby.infura.io/v3/e8bccfbf91864d7ea8797b0ae8b2d30a"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await  web3.eth.getAccounts();
+	
+	console.log("Attempting to deploy from account ", accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(compiledStore.interface))
+		.deploy({data: "0x" +compiledStore.bytecode})
+		.send({gas: "1000000", from: accounts[0]});
+
+	console.log("Whoooopiii!!!, The contract Deployed at ",result.options.address);	
+}  
+deploy();
